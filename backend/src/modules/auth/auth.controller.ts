6086ccdd0b9310ba74c9afd3c80325e7b92e2cf7
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-07-11 01:09:28 +0530

    feat: implement refresh token rotation with session management and error handling

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -8,6 +8,9 @@ import {
   generateRefreshToken,
 } from "../../utils/tokens.js";
 import { cookieOptions } from "../../utils/cookieOptions.js";
+import { hashToken } from "../../utils/hashToken.js";
+import { Session } from "../session/session.model.js";
+import jwt from "jsonwebtoken";
 
 const registerUser = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
@@ -72,6 +75,70 @@ const loginUser = asyncHandler(
       })
       .status(200)
       .json({ success: true, message: "User logged in successfully" });
+  },
+);
+
+const refreshTokenRotation = asyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const refreshToken = req.cookies?.refreshToken;
+    if (!refreshToken) throw new ApiError(400, "RefreshToken not found");
+
+    let decoded;
+
+    try {
+      decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!) as {
+        _id: string;
+      };
+    } catch (error) {
+      throw new ApiError(401, "Invalid or expired refresh token");
+    }
+
+    if (!decoded) throw new ApiError(400, "Invalid refreshToken");
+
+    const user = await User.findById(decoded._id);
+    if (!user) throw new ApiError(404, "user not found");
+
+    const refreshTokenHash = hashToken(refreshToken);
+
+    const session = await Session.findOne({
+      user: decoded._id,
+      refreshTokenHash,
+      isRevoked: false,
+    });
+
+    if (!session) {
+      await Session.updateMany(
+        { user: user._id },
+        { isRevoked: true, expiresAt: new Date() },
+      );
+      throw new ApiError(401, "Invalid refreshToken");
+    }
+
+    if (session.expiresAt < new Date())
+      throw new ApiError(401, "Session expired");
+
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+    const newRefreshTokenHash = hashToken(newRefreshToken);
+
+    session.refreshTokenHash = newRefreshTokenHash;
+    session.expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+    await session.save();
+
+    res
+      .cookie("accessToken", newAccessToken, {
+        ...cookieOptions,
+        maxAge: 15 * 60 * 1000,
+      })
+      .cookie("refreshToken", newRefreshToken, {
+        ...cookieOptions,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: "Token refreshed successfully",
+      });
   },
 );
 
