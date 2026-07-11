e352965c985cc3b0652846d8745cfaabf45453f7
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-07-11 22:25:12 +0530

    feat: add middleware for uploading files to Cloudinary

---
+++ backend/src/middlewares/multer.middleware.ts
@@ -0,0 +1,28 @@
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import ApiError from "../utils/ApiError.js";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
+
+export const uploadToCloudinary = (
+  buffer: Buffer,
+  filename: string,
+): Promise<UploadApiResponse> => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: "raw",
+        folder: "resumeForge/resumes",
+        public_id: filename,
+      },
+      (error, result) => {
+        if (error) return reject(new ApiError(500, "Failed to upload file"));
+        resolve(result!);
+      },
+    );
+    stream.end(buffer);
+  });
+};
