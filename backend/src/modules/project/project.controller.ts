e2726ff7712b476bb857cfac916c13cc1804183a
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-07-23 22:15:10 +0530

    feat: implement addProject controller and define AddProjectBody interface

---
+++ backend/src/modules/project/project.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import asyncHandler from "../../utils/asyncHandler.js";
+import { AddProjectBody } from "../../types/project.types.js";
+import ApiError from "../../utils/ApiError.js";
+import { Project } from "./project.model.js";
+
+const addProject = asyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const { title, techStack, rawData } = req.body as AddProjectBody;
+
+    if (!title.trim() || !rawData.trim())
+      throw new ApiError(400, "Title and Project data is required");
+    if (!techStack || (Array.isArray(techStack) && techStack.length === 0))
+      throw new ApiError(400, "Please enter techs used in this project.");
+
+    const sanitizedTechStack = Array.isArray(techStack)
+    ? techStack.map((t)=> t.trim()).filter(Boolean)
+    : String(techStack).split(",").map((t)=> t.trim()).filter(Boolean)
+
+    const project = await Project.create({
+      user: req.user!._id,
+      title: title.trim(),
+      techStack: sanitizedTechStack,
+      summary: "",
+      bulletPoints: [],
+      rawData,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Project added successfully.",
+      project,
+    });
+  },
+);
+
+export {addProject}
---
+++ backend/src/types/project.types.ts
@@ -0,0 +1,5 @@
+export interface AddProjectBody {
+    title: string;
+    rawData: string;
+    techStack: string[]
+}
